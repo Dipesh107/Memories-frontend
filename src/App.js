c28233fa557d2d3936ac864dc88aa5c4f6a7dd35
Pradnya Nakate <88828825+PradnyaN13@users.noreply.github.com>
2022-10-17 01:25:22 +0530

    comment changes added for app.js
    comment changes added for app.js inside socio app

--- src/App.js
+++ src/App.js
@@ -16,11 +16,17 @@ const App = () => {
       <Container maxWidth="xl">
         <Navbar />
         <Switch>
+//     route for home page
           <Route path="/" exact component={() => <Redirect to="/posts" />} />
+//   route for post page
           <Route path="/posts" exact component={Home} />
+//             route for post search page
           <Route path="/posts/search" exact component={Home} />
+//             route for post ID page
           <Route path="/posts/:id" exact component={PostDetails} />
+//             route for creator page
           <Route path={['/creators/:name', '/tags/:name']} component={CreatorOrTag} />
+//             route for auth page
           <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
         </Switch>
       </Container>
